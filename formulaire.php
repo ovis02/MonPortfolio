3de7a1e059118f826db6ba90b13b94e495f3b298
MOHAMMAD Aowis <oves7860@gmail.com>
2023-12-21 01:40:10 +0100

    configuration de la base de donnée pour le formulaire

--- formulaire.php
+++ formulaire.php
@@ -11,13 +11,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     try {
-        $servername = "localhost";
-        $dbname = "portfolio";
-        $username = "root";
-        $password = "";
+            $host = "ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+            $database = "cd9qjh84w06noeth";
+            $username = "ufmtrdqqw5rnyyih";
+            $password = "s6ovwutglzlers1f";
+            $port = 3306;
+
 
         // Crée une connexion PDO à la base de données
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
         // Prépare la requête d'insertion avec la date
